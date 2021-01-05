aab868e9613d6e881d428ed0884a34bff81ed448
srajvanshi3 <shivam99rajvanshi@gmail.com>
2021-01-05 10:27:12 +0530

    listView

--- src/Component/UserListView.js
+++ src/Component/UserListView.js
@@ -2,12 +2,21 @@ import React from 'react'
 
 function UserListView(props) {
 
-    console.log(".......props" , props)
+    console.log(".......props" , props.search[0])
+    const usersList = props.search
+    
+    usersList.map((log)=>{
+        const loginID= log.login
+        console.log("aray..", loginID)
+    })
 
     return (
         <div>
             <h3>search result: </h3>
-            <li></li>
+
+       {usersList.map((log)=>{
+           return <li>{log.login}</li>
+       })}
         </div>
     )
 }
